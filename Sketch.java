52b654972e38069925c340628e9fd789613ffcda
Oscar Lin <78488962+oscarlin05@users.noreply.github.com>
2022-02-25 01:20:19 +0000

    updated programming style

--- Sketch.java
+++ Sketch.java
@@ -28,28 +28,28 @@ public class Sketch extends PApplet {
     int scaledwidth = width/400;
     int scaledheight = height/400;
 
-	//ground color (foreground)
+	// ground color (foreground)
     stroke(74, 50, 1);
     fill(74, 50, 1);
     rect(0 * scaledwidth, 325 * scaledheight, 400 * scaledwidth, 100 * scaledheight);
     
-  //lawn (foreground)
+  // lawn (foreground)
     stroke(0);
     fill(22, 181, 72);
     rect(75 * scaledwidth, 325 * scaledheight, 250 * scaledwidth, 250 * scaledheight);
     //rect(600, 650, 250, 50);
 
-  //pathway (foreground)
+  // pathway (foreground)
     stroke(0);
     fill(130, 148, 148);
     rect(195 * scaledwidth, 325 * scaledheight, 40 * scaledwidth, 100 * scaledheight);    
     
-  //house shape
+  // house shape
     stroke(0);
     fill(247, 186, 62);
     rect(125 * scaledwidth, 150 * scaledheight, 175 * scaledwidth, 175 * scaledheight);
 
-  //roof
+  // roof
     stroke(0);
     fill(153, 99, 0);
     triangle(75 * scaledwidth, 150 * scaledheight, 212 * scaledwidth , 50 * scaledheight, 350 * scaledwidth, 150 * scaledheight);
@@ -60,16 +60,16 @@ public class Sketch extends PApplet {
     rect(195 * scaledwidth, 275 * scaledheight, 40 * scaledwidth, 50 * scaledheight);
     ellipse(230 * scaledwidth, 305 * scaledheight, 7 * scaledwidth, 7 * scaledheight);
 
-  //window
+  // window
     stroke(0);
     fill(185, 250, 249);
     rect(195 * scaledwidth , 175 * scaledheight, 40 * scaledwidth, 40 * scaledheight);
     
-    //line(390, 390, 470, 390);
+    // line(390, 390, 470, 390);
     line(195 * scaledwidth, 195 * scaledheight, 235 * scaledwidth, 195 * scaledheight);
     line(215 * scaledwidth, 175 * scaledheight, 215 * scaledwidth, 215* scaledheight);
     
-    //sun
+    // sun
     stroke(0);
     fill(250, 225, 0);
     ellipse(375 * scaledwidth, 25 * scaledheight, 50 * scaledwidth, 50 * scaledheight);
